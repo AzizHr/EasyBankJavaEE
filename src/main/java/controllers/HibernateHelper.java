7e4b8130f19a824c948e9b5bf36830e1f5b56433
Haytam Beniazza <beniazza0@gmail.com>
2023-10-24 10:14:55 +0100

    search client

--- src/main/java/controllers/HibernateHelper.java
+++ src/main/java/controllers/HibernateHelper.java
@@ -14,9 +14,9 @@ public class HibernateHelper {
 
             // Configure Hibernate properties
             configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/easy_bank");
+            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/easy_bank1");
             configuration.setProperty("hibernate.connection.username", "postgres");
-            configuration.setProperty("hibernate.connection.password", "aziz");
+            configuration.setProperty("hibernate.connection.password", "1234");
             configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
             configuration.setProperty("hibernate.show_sql", "true");
             configuration.setProperty("hbm2ddl.auto", "update");
@@ -25,15 +25,15 @@ public class HibernateHelper {
             // Add annotated classes
             configuration.addAnnotatedClass(Client.class);
             configuration.addAnnotatedClass(Employee.class);
-            configuration.addAnnotatedClass(Demand.class);
-            configuration.addAnnotatedClass(CurrentAccount.class);
-            configuration.addAnnotatedClass(Agency.class);
-            configuration.addAnnotatedClass(Mission.class);
-            configuration.addAnnotatedClass(MissionHistory.class);
-            configuration.addAnnotatedClass(Operation.class);
-            configuration.addAnnotatedClass(Payment.class);
-            configuration.addAnnotatedClass(RecruitmentHistory.class);
-            configuration.addAnnotatedClass(SavingAccount.class);
+//            configuration.addAnnotatedClass(Demand.class);
+//            configuration.addAnnotatedClass(CurrentAccount.class);
+//            configuration.addAnnotatedClass(Agency.class);
+//            configuration.addAnnotatedClass(Mission.class);
+//            configuration.addAnnotatedClass(MissionHistory.class);
+//            configuration.addAnnotatedClass(Operation.class);
+//            configuration.addAnnotatedClass(Payment.class);
+//            configuration.addAnnotatedClass(RecruitmentHistory.class);
+//            configuration.addAnnotatedClass(SavingAccount.class);
 
             sessionFactory = configuration.buildSessionFactory();
         }
